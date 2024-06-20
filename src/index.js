8e1fd7337a8ed44ca8695e6a31ef81f8256c630d
bitepeng <bitepeng@qq.com>
2024-06-20 16:33:52 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -7,16 +7,16 @@ const dockerHub = "https://registry-1.docker.io";
 
 const routes = {
   // production
-  "docker.libcuda.so": dockerHub,
-  "quay.libcuda.so": "https://quay.io",
-  "gcr.libcuda.so": "https://gcr.io",
-  "k8s-gcr.libcuda.so": "https://k8s.gcr.io",
-  "k8s.libcuda.so": "https://registry.k8s.io",
-  "ghcr.libcuda.so": "https://ghcr.io",
-  "cloudsmith.libcuda.so": "https://docker.cloudsmith.io",
+  "docker.hub.miaoxue.top": dockerHub,
+  "quay.hub.miaoxue.top": "https://quay.io",
+  "gcr.hub.miaoxue.top": "https://gcr.io",
+  "k8s-gcr..hub.miaoxue.top": "https://k8s.gcr.io",
+  "k8s.hub.miaoxue.top": "https://registry.k8s.io",
+  "ghcr.hub.miaoxue.top": "https://ghcr.io",
+  "cloudsmith.hub.miaoxue.top": "https://docker.cloudsmith.io",
 
   // staging
-  "docker-staging.libcuda.so": dockerHub,
+  "docker-staging.hub.miaoxue.top": dockerHub,
 };
 
 function routeByHosts(host) {
